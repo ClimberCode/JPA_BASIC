f0a9ff0c1843e3801436c3ee2beebb9a53eedf1a
ClimberCode <107402848+ClimberCode@users.noreply.github.com>
2023-11-07 23:18:55 +0900

    first commit
    JPA study 꾸준히 공부해보자

---
+++ jpa-basic/src/main/java/hello/Member.java
@@ -0,0 +1,84 @@
+package hello;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+    @Column(name="name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedDate;
+
+    @Lob
+    private String descreption;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public Member () {}
+
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getDescreption() {
+        return descreption;
+    }
+
+    public void setDescreption(String descreption) {
+        this.descreption = descreption;
+    }
+}
